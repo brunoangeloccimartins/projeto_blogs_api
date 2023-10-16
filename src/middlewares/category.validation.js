32a9937491d59375e7cd19f5face7896330e3eee
brunoangeloccimartins <brunoangelocci@gmail.com>
2023-10-16 18:37:43 -0300

    Req 15

--- src/middlewares/category.validation.js
+++ src/middlewares/category.validation.js
@@ -4,9 +4,10 @@ const categoryValidation = async (req, res, next) => {
   const { categoryIds } = req.body;
   const category = await Category.findAll();
   const categoriesIds = category.map((cat) => cat.id);
+  if (!categoryIds) return next();
   const categoryIdsNotFound = categoryIds.filter((cat) => !categoriesIds.includes(cat));
   if (categoryIdsNotFound.length > 0) {
-    return res.status(400).json({ message: 'one or more "categoryIds" not found' });
+    return res.status(400).json({ message: 'One or more "categoryIds" not found' });
   }
   next();
 };
